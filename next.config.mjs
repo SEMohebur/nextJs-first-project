4d16c8849fbad00c7782a7519c5d34b2ac3528c4
MD. MOHIBUR <md.mohibur@Moheburs-MacBook-Air.local>
2025-11-25 17:59:24 +0600

    next.config.mjs update/image url error fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,19 +1,26 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
   reactCompiler: true,
   images: {
-    domains: [
-      "images.unsplash.com",
-      "plus.unsplash.com",
-      "www.pexels.com",
-      "images.pexels.com",
-      "picsum.photos",
-      "via.placeholder.com",
-      "www.applegadgetsbd.com",
-      "adminapi.applegadgetsbd.com",
-      "cdn.pixabay.com",
-      "upload.wikimedia.org",
+    remotePatterns: [
+      { protocol: "https", hostname: "images.unsplash.com", pathname: "/**" },
+      { protocol: "https", hostname: "plus.unsplash.com", pathname: "/**" },
+      { protocol: "https", hostname: "www.pexels.com", pathname: "/**" },
+      { protocol: "https", hostname: "images.pexels.com", pathname: "/**" },
+      { protocol: "https", hostname: "picsum.photos", pathname: "/**" },
+      { protocol: "https", hostname: "via.placeholder.com", pathname: "/**" },
+      {
+        protocol: "https",
+        hostname: "www.applegadgetsbd.com",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "adminapi.applegadgetsbd.com",
+        pathname: "/**",
+      },
+      { protocol: "https", hostname: "cdn.pixabay.com", pathname: "/**" },
+      { protocol: "https", hostname: "upload.wikimedia.org", pathname: "/**" },
     ],
   },
 };
